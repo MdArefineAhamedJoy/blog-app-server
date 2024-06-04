85c2b4a1feb3872fa4a4f78f69b935b62a5f3c74
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2024-06-04 16:53:35 +0600

    create server and databased

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const sequelize = require('./src/config/database');
+// const authRoutes = require('./routes/authRoutes');
+// const blogRoutes = require('./routes/blogRoutes');
+// const userRoutes = require('./routes/userRoutes');
+// const User = require('./models/user');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get('/', async (req, res) => {
+    res.send('Welcome to my-block-app server')
+})
+
+const PORT = process.env.PORT || 5000;
+
+sequelize.sync().then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
